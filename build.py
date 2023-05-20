cca52ac07bbb322578fc226911f1e3298c615161
Janagan Gunasekaran <janagan.gunasekaran@hpe.com>
2023-05-20 17:30:49 +0530

    Update build.py

--- build.py
+++ build.py
@@ -8,7 +8,9 @@ def build_packages():
         if not os.path.exists(f'{DIRPATH}/build_packages/'):
             os.makedirs(f'{DIRPATH}/build_packages/')
         command = f'docker run --rm -v {DIRPATH}:/local wework/speccy resolve /local{i[1]} -o /local/build_packages/{filename}.yaml'
+        print(command)
         output = subprocess.run(command.split(), shell=True)
 
 if __name__ == "__main__":
+    print(DIRPATH)
     build_packages()
