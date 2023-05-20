4be27f05d6b4472652f82e94803c66592261579d
Janagan Gunasekaran <janagan.gunasekaran@hpe.com>
2023-05-20 15:37:02 +0530

    path changes

--- test/data_test.py
+++ test/data_test.py
@@ -6,7 +6,7 @@ from ..config import DIRPATH
 class TestStringMethods(unittest.TestCase):
 
     def test_data_json(self):
-        data = get_data(f"{DIRPATH}\\src\\data.json")
-        schema = get_schema(f"{DIRPATH}\\build_packages\\base.yaml")
+        data = get_data(f"{DIRPATH}/src/data.json")
+        schema = get_schema(f"{DIRPATH}/build_packages/base.yaml")
         validate = validate_json(data, schema)
         self.assertEqual(validate, True)
